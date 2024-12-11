74d822de869c775a849cc0ba3a7e36b9ed7e6a02
p-torrecilla <p.torrecilla.2021@alumnos.urjc.es>
2024-12-11 16:04:40 +0100

    Heat map and decision tree with new variables

---
+++ decision_tree_modified.py
@@ -0,0 +1,67 @@
+# Import libraries to work with CSV files and to graph the trees
+import pandas as pd
+from sklearn import tree
+import graphviz
+
+# Read from the tek CSV file
+data = pd.read_csv('malware-detection/tek_data.csv')
+
+# Define features and target
+columns = [
+    "AddressOfEntryPoint",
+    "BaseOfData",
+    "Characteristics",
+    "CheckSum",
+    "ExportNb",
+    "FileAlignment",
+    "ImageBase",
+    "ImportsNbDLL",
+    "ImportsNbOrdinal",
+    "LoadConfigurationSize",
+    #"Machine",
+    "MajorImageVersion",
+    "MajorOperatingSystemVersion",
+    "MinorOperatingSystemVersion",
+    "ResourcesMeanEntropy",
+    "ResourcesMeanSize",
+    "ResourcesMinEntropy",
+    "SectionsMeanRawsize",
+    "SectionsMinRawsize",
+    "SectionsNb",
+    "SizeOfCode",
+    "SizeOfHeaders",
+    "SizeOfHeapReserve",
+    "SizeOfStackCommit",
+    "SizeOfUninitializedData",
+    "Subsystem"
+]
+X = data[columns]
+y = data['legitimate']
+
+# Train the model and set the seed as 1410
+clf = tree.DecisionTreeClassifier(random_state=1410)
+clf = clf.fit(X, y)
+
+# Print plot
+
+dot_data = tree.export_graphviz(clf, out_file=None,
+    feature_names=X.columns,
+    class_names=["Malware", "Legitimate"],
+    filled=True, rounded=True,
+    special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render('tek_tree_cleaned_variables', view=True).replace('\\', '/')
+
+
+# Predict the data
+predict_data = pd.read_csv('malware-detection/test_data.csv', header=0)
+predict_data = predict_data[columns]
+print(predict_data)
+prediction = clf.predict(predict_data)
+
+# Read the ID from the test CSV file
+data_2 = pd.read_csv('malware-detection/test_data.csv', usecols=["ID"])
+
+#Creates the column for the predictions
+data_2["Prediction"] = prediction
+data_2.to_csv('Delta.csv', index=False)
---
+++ heat_map.py
@@ -0,0 +1,61 @@
+#importing libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import RidgeCV, LassoCV, Ridge, Lasso
+
+# Threshold variable for the correlation decision
+threshold = 0.5
+
+# Read from the tek CSV file
+data = pd.read_csv('malware-detection/tek_data.csv')
+
+# Define features and target
+columns = [
+    "AddressOfEntryPoint",
+    "BaseOfData",
+    "Characteristics",
+    "CheckSum",
+    "ExportNb",
+    "FileAlignment",
+    "ImageBase",
+    "ImportsNbDLL",
+    "ImportsNbOrdinal",
+    "LoadConfigurationSize",
+    "MajorImageVersion",
+    "MajorOperatingSystemVersion",
+    "MinorOperatingSystemVersion",
+    "ResourcesMeanEntropy",
+    "ResourcesMeanSize",
+    "ResourcesMinEntropy",
+    "SectionsMeanRawsize",
+    "SectionsMinRawsize",
+    "SectionsNb",
+    "SizeOfCode",
+    "SizeOfHeaders",
+    "SizeOfHeapReserve",
+    "SizeOfStackCommit",
+    "SizeOfUninitializedData",
+    "Subsystem"
+]
+X = data[columns]
+
+#Using Pearson Correlation
+plt.figure(figsize=(12,10))
+cor = X.corr()
+
+""""
+#Correlation with output variable
+cor_target = abs(cor["legitimate"])
+#Selecting highly correlated features
+relevant_features = cor_target[cor_target>threshold]
+print(relevant_features)
+"""
+#Printing the map
+sns.heatmap(cor, annot=False, cmap=plt.cm.Reds)
+plt.show()
